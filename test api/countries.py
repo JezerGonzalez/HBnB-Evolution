d26c09f81c836e0622235fd7dd5c04a9f3a2e69c
Gadiel02 <gadielrivera02@gmail.com>
2024-06-13 20:42:36 -0400

    t

--- test api/countries.py
+++ test api/countries.py
@@ -1,42 +1,32 @@
-from flask import Blueprint, request, jsonify
+from flask import Flask, jsonify, abort
+from class_country import Country
 
-countries_bp = Blueprint('countries', __name__)
+app = Flask(__name__)
 
-countries_db = {}
-country_id_counter = 1
 
-@countries_bp.route('/', methods=['POST'])
-def create_country():
-    global country_id_counter
-    data = request.get_json()
-    data['id'] = country_id_counter
-    countries_db[country_id_counter] = data
-    country_id_counter += 1
-    return jsonify(data), 201
+places = [
+    {'id': 1, 'name': 'Place A', 'country_code': 'US', 'description': 'Description of Place A'},
+    {'id': 2, 'name': 'Place B', 'country_code': 'CA', 'description': 'Description of Place B'}
+]
 
-@countries_bp.route('/', methods=['GET'])
+
+@app.route('/places', methods=['GET'])
+def get_all_places():
+    return jsonify({'places': places})
+
+
+@app.route('/places/<int:place_id>', methods=['GET'])
+def get_place_by_id(place_id):
+    place = next((p for p in places if p['id'] == place_id), None)
+    if place is None:
+        abort(404, description="Place not found.")
+    return jsonify({'place': place})
+
+
+@app.route('/countries', methods=['GET'])
 def get_countries():
-    return jsonify(list(countries_db.values())), 200
+    countries = Country.get_all_countries()
+    return jsonify({'countries': countries})
 
-@countries_bp.route('/<int:country_id>', methods=['GET'])
-def get_country(country_id):
-    country = countries_db.get(country_id)
-    if not country:
-        return jsonify({"error": "Country not found"}), 404
-    return jsonify(country), 200
-
-@countries_bp.route('/<int:country_id>', methods=['PUT'])
-def update_country(country_id):
-    if country_id not in countries_db:
-        return jsonify({"error": "Country not found"}), 404
-    data = request.get_json()
-    data['id'] = country_id
-    countries_db[country_id] = data
-    return jsonify(data), 200
-
-@countries_bp.route('/<int:country_id>', methods=['DELETE'])
-def delete_country(country_id):
-    if country_id not in countries_db:
-        return jsonify({"error": "Country not found"}), 404
-    del countries_db[country_id]
-    return '', 204
+if __name__ == '__main__':
+    app.run(debug=True)
